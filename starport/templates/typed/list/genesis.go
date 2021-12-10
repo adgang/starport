4fbdf70de6286e1620c70be65b07bc14465fa43e
adgang <adgang@tuta.io>
2021-12-10 10:41:10 +0530

    removed replacer from genesis types modify

--- starport/templates/typed/list/genesis.go
+++ starport/templates/typed/list/genesis.go
@@ -14,7 +14,7 @@ import (
 
 func genesisModify(replacer placeholder.Replacer, opts *typed.Options, g *genny.Generator) {
 	g.RunFn(genesisProtoModify(replacer, opts))
-	g.RunFn(genesisTypesModify(replacer, opts))
+	g.RunFn(genesisTypesModify(opts))
 	g.RunFn(genesisModuleModify(replacer, opts))
 	g.RunFn(genesisTestsModify(replacer, opts))
 	g.RunFn(genesisTypesTestsModify(replacer, opts))
@@ -66,7 +66,7 @@ func genesisProtoModify(replacer placeholder.Replacer, opts *typed.Options) genn
 	}
 }
 
-func genesisTypesModify(replacer placeholder.Replacer, opts *typed.Options) genny.RunFn {
+func genesisTypesModify(opts *typed.Options) genny.RunFn {
 
 	return func(r *genny.Runner) error {
 		path := filepath.Join(opts.AppPath, "x", opts.ModuleName, "types/genesis.go")
@@ -88,29 +88,27 @@ func genesisTypesModify(replacer placeholder.Replacer, opts *typed.Options) genn
 
 		typed.AddKeysToDefaultGenesisState(dstHelper, key, typeName)
 
-		templateTypesValidate := `// Check for duplicated ID in %[2]v
-%[2]vIdMap := make(map[uint64]bool)
-%[2]vCount := gs.Get%[3]vCount()
-for _, elem := range gs.%[3]vList {
-	if _, ok := %[2]vIdMap[elem.Id]; ok {
-		return fmt.Errorf("duplicated id for %[2]v")
+		templateTypesValidate := `// Check for duplicated ID in %[1]v
+%[1]vIdMap := make(map[uint64]bool)
+%[2]vCount := gs.Get%[2]vCount()
+for _, elem := range gs.%[2]vList {
+	if _, ok := %[1]vIdMap[elem.Id]; ok {
+		return fmt.Errorf("duplicated id for %[1]v")
 	}
-	if elem.Id >= %[2]vCount {
-		return fmt.Errorf("%[2]v id should be lower or equal than the last id")
+	if elem.Id >= %[1]vCount {
+		return fmt.Errorf("%[1]v id should be lower or equal than the last id")
 	}
-	%[2]vIdMap[elem.Id] = true
+	%[1]vIdMap[elem.Id] = true
 }
 `
 		replacementTypesValidate := fmt.Sprintf(
 			templateTypesValidate,
-			typed.PlaceholderGenesisTypesValidate,
 			opts.TypeName.LowerCamel,
 			opts.TypeName.UpperCamel,
 		)
 
 		typed.AddGenesisStateValidation(dstHelper, replacementTypesValidate)
 
-		// panic(1)
 		content, err := dstHelper.Content()
 
 		if err != nil {
